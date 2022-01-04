914114077cd2b4ff1a1b5fb7c6410052cd049b07
nopara73 <adam.ficsor73@gmail.com>
2022-01-04 13:50:05 +0100

    Add comment

--- Sake/Mixer.cs
+++ Sake/Mixer.cs
@@ -19,6 +19,8 @@ namespace Sake
             OutputSize = outputSize;
 
             MinAllowedOutputAmountPlusFee = minAllowedOutputAmount + OutputFee;
+
+            // Create many standard denominations.
             DenominationsPlusFees = CreateDenominationsPlusFees();
         }
 
